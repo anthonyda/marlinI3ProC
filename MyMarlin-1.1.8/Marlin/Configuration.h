2ed07b326e644bdcce452cd1a7b61c3d4eb6cf80
anthonyDa <anthonydam@msn.com>
2018-04-14 17:01:23 +0200

    lowered jerk speed to avoid layer shifting

--- MyMarlin-1.1.8/Marlin/Configuration.h
+++ MyMarlin-1.1.8/Marlin/Configuration.h
@@ -582,7 +582,7 @@
  */
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -593,7 +593,7 @@
  * value set here, it may happen instantaneously.
  */
 #define DEFAULT_XJERK                 20.0
-#define DEFAULT_YJERK                 20.0
+#define DEFAULT_YJERK                 10.0
 #define DEFAULT_ZJERK                  0.4
 #define DEFAULT_EJERK                  5.0
 
